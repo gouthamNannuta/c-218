1e848323f33149861df116c0127fad322013d7f8
gouthamNannuta <gouthamenterprises.hnk@gmail.com>
2022-07-07 19:27:10 +0530

    upload-8

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const { json } = require("express");
+
 const socket = io("/");
 
 var peer = new Peer(undefined, {
@@ -100,6 +102,28 @@ $(function () {
             $("#stop_video").toggleClass("background_red");
             $("#stop_video").html(html)
         }
+    })
+
+    $("#invite_button").click(function(){
+        const to = prompt("enter the email address")
+        let data = {
+            url:window.location.href,
+            to:to,
+        }
+        $.ajax({
+            url :"/send-mail",
+            type: "post",
+            data: JSON.stringify(data),
+            dataType : "json",
+            contentType:"application/json",
+            success: function(result){
+                alert("invite sent!!!")
+            },
+            error:function(result){
+                console.log(result.responseJSON)
+            }
+
+        })
     })
 })
 
